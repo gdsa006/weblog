ad9cb83baf1e446cb681c506b67c60902b5d15d1
Gagan Ahuja <gdsa006@ggmail.com>
2020-10-07 23:35:58 +0530

    fixes nav

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -11,17 +11,17 @@
                     </div>
 
                     <div class="navigate" data-aos="fade-down">
-                        <nav class="nav">
+                        <nav class="{{ (Request::is('login') || Request::is('admin/*') || Request::is('resources') ? 'nav2' : 'nav') }}">
                             <div class="nav-mobile">
                                 <i class="fas fa-bars"></i>
                             </div>
                             <ul>
-                                <li><a href="#videoBanner">Home</a></li>
-                                <li><a href="#who-we-are">WHO WE ARE</a></li>
-                                <li><a href="#what-we-do">WHAT WE DO</a></li>
-                                <li><a href="#getHelp">GET HELP</a></li>
-                                <li><a href="#news">NEWS</a></li>
-                                <li><a href="/resources">RESOURCES</a></li>
+                                <li><a href="{{ (Request::is('login') || Request::is('admin/*') || Request::is('resources') ? '/' : '#videoBanner') }}">Home</a></li>
+                                <li><a href="{{ (Request::is('login') || Request::is('admin/*') || Request::is('resources') ? '/#who-we-are' : '#who-we-are') }}">WHO WE ARE</a></li>
+                                <li><a href="{{ (Request::is('login') || Request::is('admin/*') || Request::is('resources') ? '/#what-we-do' : '#what-we-do') }}">WHAT WE DO</a></li>
+                                <li><a href="{{ (Request::is('login') || Request::is('admin/*') || Request::is('resources') ? '/#getHelp' : '#getHelp') }}">GET HELP</a></li>
+                                <li><a href="{{ (Request::is('login') || Request::is('admin/*') || Request::is('resources') ? '/#news' : '#news') }}">NEWS</a></li>
+                                <li><a href="/resources" class="{{ Request::is('resources') ? 'active' : '' }}">RESOURCES</a></li>
                                 <li class=" logIn"><a data-id="8EoSXrHjjk0" data-toggle="modal" data-target=".bd-example-modal-xl" href="" data-scrolltime="1500">Join Now</a></li>
                             </ul>
                         </nav>
